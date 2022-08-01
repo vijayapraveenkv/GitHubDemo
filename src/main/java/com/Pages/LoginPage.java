c54b7b529f6911b06a1f4e5037cd0431678031c9
praveen kumar <ELCOT@Lenova>
2022-08-01 08:47:43 -0700

    first commit

---
+++ src/main/java/com/Pages/LoginPage.java
@@ -0,0 +1,12 @@
+package com.Pages;
+
+public class LoginPage {
+	
+	public void loginpage() {
+		System.out.println("This is loginpage");
+		
+	}
+	
+	
+
+}
---
+++ src/test/java/com/Testcases/LoginPageTest.java
@@ -0,0 +1,8 @@
+package com.Testcases;
+
+public class LoginPageTest {
+
+	public void logintest() {
+		System.out.println("This is login page Test"); 
+	}
+}
